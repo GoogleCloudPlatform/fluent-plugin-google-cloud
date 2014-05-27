2da7181516cb8049debe29388882bcf392b89299
Alex Robinson <arob@google.com>
2014-05-27 21:50:58 +0000

    Grab all necessary metadata on both GCE and Managed VMs, distinguishing between the two automatically, and send the logs to the API. This has been tested to work for now, but will break once authentication is added to the API.

--- lib/fluent/plugin/out_google_cloud.rb
+++ lib/fluent/plugin/out_google_cloud.rb
@@ -6,6 +6,7 @@ module Fluent
       super
       require 'google/api_client'
       require 'google/api_client/auth/compute_service_account'
+      require 'rest_client'
     end
 
     def configure(conf)
@@ -16,9 +17,32 @@ module Fluent
       super
 
       @client = nil
-      # TODO: Uncomment when the logging API exists
+      # TODO: Switch over to using api client when the logs API is discoverable.
       #@api = api_client().discovered_api('logs', 'v1')
       puts api_client()
+
+      # Grab metadata about the Google Compute Engine instance that we're on.
+      @project_id = fetch_metadata('project/numeric-project-id')
+      fully_qualified_zone = fetch_metadata('instance/zone')
+      @zone = fully_qualified_zone.rpartition('/')[2]
+      @vm_id = fetch_metadata('instance/id')
+      # TODO: Send instance tags and/or hostname with the logs as well?
+
+      # If this is running on a Managed VM, grab the relevant App Engine
+      # metadata as well.
+      # TODO: Use a configuration flag instead of detecting automatically?
+      attributes_string = fetch_metadata('instance/attributes/')
+      attributes = attributes_string.split
+      if attributes.include?('gae_backend_name') &&
+         attributes.include?('gae_backend_version')
+        @running_on_managed_vm = true
+        @gae_backend_name =
+          fetch_metadata('instance/attributes/gae_backend_name')
+        @gae_backend_version =
+          fetch_metadata('instance/attributes/gae_backend_version')
+      else
+        @running_on_managed_vm = false
+      end
     end
 
     def shutdown
@@ -26,16 +50,49 @@ module Fluent
     end
 
     def format(tag, time, record)
-      [time, record['message']].to_msgpack
+      [tag, time, record['message']].to_msgpack
     end
 
     def write(chunk)
-      # TODO: Construct the payload and actually send the logs to the API
-      puts api_client()
-      chunk.msgpack_each do |row_object|
-        puts "Time: #{row_object[0]}"
-        puts row_object[1]
+      payload = {
+        'metadata' => {
+          'project' => @project_id,
+          'location' => @zone
+        }
+      }
+      if @running_on_managed_vm
+        payload['metadata']['appEngine'] = {
+          'moduleId' => @gae_backend_name,
+          'versionId' => @gae_backend_version,
+          'computeEngineVmId' => @vm_id
+        }
+      else
+        payload['metadata']['computeEngine'] = { 'instanceId' => @vm_id }
       end
+
+      # TODO: Add in calls for creating log streams?
+      chunk.msgpack_each do |row_object|
+        # Ignore the extra info that can be automatically appended to the tag
+        # for certain log types such as syslog.
+        log_name = row_object[0].partition('.')[0]
+        url = "https://www.googleapis.com/logs/v1beta/projects/#{@project_id}/logs/#{log_name}/entries"
+        payload['metadata']['timeNanos'] = row_object[1] * 1000000000
+        payload['textLogEntry'] = row_object[2]
+        
+        # TODO: Remove print statements
+        puts url
+        puts payload
+        RestClient.post(url, payload.to_json,
+                        {'Content-Type'=>'application/json'})
+      end
+    end
+
+    def fetch_metadata(metadata_path)
+      RestClient.get('http://metadata/computeMetadata/v1/' + metadata_path,
+                     {'Metadata-Flavor' => 'Google'})
+    end
+
+    def init_payload()
     end
 
     def api_client
