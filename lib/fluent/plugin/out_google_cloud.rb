GoogleCloudPlatform/fluent-plugin-google-cloud

commit 47aa2e5c779175fff52189af5402a8d669705be0

Drop the batch on failure to serialize the payload
This is a temporary workaround to stop us from getting stuck
in an infinite loop if we ingest non-7-bit ASCII data; now we
will drop the batch of log entries instead.

lib/fluent/plugin/out_google_cloud.rb
@@ -208,6 +208,8 @@ module Fluent
           if (error.message == 'Invalid Credentials')
             raise error
           end
+          # fall through to next rescue clause
+        rescue JSON::GeneratorError => error
           dropped = write_log_entries_request['entries'].length
           $log.warn "Dropping #{dropped} log message(s)",
               :error_class=>error.class.to_s, :error=>error.to_s
@@ -227,7 +229,7 @@ module Fluent
     def init_api_client
       @client = Google::APIClient.new(
         :application_name => 'Fluentd Google Cloud Logging plugin',
-        :application_version => '0.1.2',
+        :application_version => '0.1.3',
         :retries => 1)
 
       if @auth_method == 'private_key'

fluent-plugin-google-cloud.gemspec
@@ -4,7 +4,7 @@ Gem::Specification.new do |gem|
   gem.summary       = %q{Fluentd plugin to stream logs to the Google Cloud Platform's logging API}
   gem.homepage      = 'https://github.com/GoogleCloudPlatform/fluent-plugin-google-cloud'
   gem.license       = 'Apache 2.0'
-  gem.version       = '0.1.2'
+  gem.version       = '0.1.3'
   gem.authors       = ['Todd Derr', 'Alex Robinson']
   gem.email         = ['salty@google.com']
 

commit c6c805ff53c8378642b39ad00b8d52b782cea987

Log dropped messages due to ClientError.
Ruby exceptions don't actually fall-through, so factor out the logging
code and call it on ClientError instead.

This bug was inadvertently introduced in commit
47aa2e5c779175fff52189af5402a8d669705be0

lib/fluent/plugin/out_google_cloud.rb
@@ -199,16 +199,21 @@ module Fluent
           if (error.message == 'Invalid Credentials')
             raise error
           end
-          # fall through to next rescue clause
+          log_write_failure(write_log_entries_request, error)
         rescue JSON::GeneratorError => error
           dropped = write_log_entries_request['entries'].length
-          $log.warn "Dropping #{dropped} log message(s)",
-              :error_class=>error.class.to_s, :error=>error.to_s
+          log_write_failure(write_log_entries_request, error)
         end
       end
     end
 
     private
+
+    def log_write_failure(request, error)
+      dropped = request['entries'].length
+      $log.warn "Dropping #{dropped} log message(s)",
+        :error_class=>error.class.to_s, :error=>error.to_s
+    end
 
     def fetch_metadata(metadata_path)
       open('http://metadata/computeMetadata/v1/' + metadata_path,