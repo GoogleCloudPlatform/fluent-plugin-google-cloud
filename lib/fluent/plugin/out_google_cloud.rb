83e66bc4f1d277a66d06d6e31133f0bc9ea4da33
Todd Derr <salty@google.com>
2015-12-17 13:35:42 -0500

    version 0.4.13

--- lib/fluent/plugin/out_google_cloud.rb
+++ lib/fluent/plugin/out_google_cloud.rb
@@ -787,7 +787,7 @@ module Fluent
     def init_api_client
       @client = Google::APIClient.new(
         application_name: 'Fluentd Google Cloud Logging plugin',
-        application_version: '0.4.12',
+        application_version: '0.4.13',
         retries: 1)
 
       if @auth_method == 'private_key'
