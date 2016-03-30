395ced24e2718103341e9a8d61ebf6aab5ae2911
Laramie Leavitt <lar@google.com>
2016-03-30 11:43:15 -0700

    Revert version to 0.4.15

--- lib/fluent/plugin/out_google_cloud.rb
+++ lib/fluent/plugin/out_google_cloud.rb
@@ -26,7 +26,7 @@ module Fluent
     Fluent::Plugin.register_output('google_cloud', self)
 
     PLUGIN_NAME = 'Fluentd Google Cloud Logging plugin'
-    PLUGIN_VERSION = '0.4.16'
+    PLUGIN_VERSION = '0.4.15'
 
     # Constants for service names.
     APPENGINE_SERVICE = 'appengine.googleapis.com'
