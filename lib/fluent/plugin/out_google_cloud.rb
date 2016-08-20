f8b0345d14405f16921dff9e53bb9b38900b4241
Tony Meng <tonymeng@users.noreply.github.com>
2016-08-19 20:09:38 -0500

    Map NGINX error log levels (#80)

--- lib/fluent/plugin/out_google_cloud.rb
+++ lib/fluent/plugin/out_google_cloud.rb
@@ -716,6 +716,9 @@ module Fluent
       'FINE' => 'DEBUG',
       'FINER' => 'DEBUG',
       'FINEST' => 'DEBUG',
+      # nginx levels (only missing ones from above listed).
+      'CRIT' => 'CRITICAL',
+      'EMERG' => 'EMERGENCY',
       # single-letter levels.  Note E->ERROR and D->DEBUG.
       'D' => 'DEBUG',
       'I' => 'INFO',
