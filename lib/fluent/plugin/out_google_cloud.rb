b5ebce6a33f6e109e67d8f4300779d649d89d951
Igor Peshansky <igorpeshansky@github.com>
2017-11-22 17:43:36 -0500

    Handle non-string severity values in the incoming JSON.

--- lib/fluent/plugin/out_google_cloud.rb
+++ lib/fluent/plugin/out_google_cloud.rb
@@ -1454,7 +1454,7 @@ module Fluent
 
     def parse_severity(severity_str)
       # The API is case insensitive, but uppercase to make things simpler.
-      severity = severity_str.upcase.strip
+      severity = severity_str.to_s.upcase.strip
 
       # If the severity is already valid, just return it.
       return severity if VALID_SEVERITIES.include?(severity)
