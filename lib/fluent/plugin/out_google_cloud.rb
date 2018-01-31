cbbb78dcb2e36c710fdb0d413917d42580ffd0ae
Ling Huang <lingshi@google.com>
2018-01-31 16:50:40 -0500

    Removed obsolete comment of partial success support availability.

--- lib/fluent/plugin/out_google_cloud.rb
+++ lib/fluent/plugin/out_google_cloud.rb
@@ -307,8 +307,7 @@ module Fluent
 
     # Whether valid entries should be written even if some other entries fail
     # due to INVALID_ARGUMENT or PERMISSION_DENIED errors when communicating to
-    # the Stackdriver Logging API. This is highly recommended. Right now this
-    # only works with the REST path (use_grpc = false).
+    # the Stackdriver Logging API. This is highly recommended.
     config_param :partial_success, :bool, :default => false
 
     # Whether to allow non-UTF-8 characters in user logs. If set to true, any
