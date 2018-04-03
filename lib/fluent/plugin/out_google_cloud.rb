34bab5bc08f5094762aba27c10314a3b98c3cab2
Ling Huang <qingling128@users.noreply.github.com>
2018-04-03 12:51:33 +0200

    Bump version to v0.6.18. (#218)

--- lib/fluent/plugin/out_google_cloud.rb
+++ lib/fluent/plugin/out_google_cloud.rb
@@ -226,7 +226,7 @@ module Fluent
     Fluent::Plugin.register_output('google_cloud', self)
 
     PLUGIN_NAME = 'Fluentd Google Cloud Logging plugin'.freeze
-    PLUGIN_VERSION = '0.6.17'.freeze
+    PLUGIN_VERSION = '0.6.18'.freeze
 
     # Name of the the Google cloud logging write scope.
     LOGGING_SCOPE = 'https://www.googleapis.com/auth/logging.write'.freeze
