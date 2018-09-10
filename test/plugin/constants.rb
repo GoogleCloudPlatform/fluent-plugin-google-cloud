a10074274d91eea2248a42267130ca3c4f77d76d
Ling Huang <lingshi@google.com>
2018-09-10 16:00:35 -0400

    fix travis

--- test/plugin/constants.rb
+++ test/plugin/constants.rb
@@ -387,10 +387,6 @@ module Constants
       }
     )
   ).freeze
-  # Used in k8s fallback tests.
-  COMPUTE_PARAMS_WITH_CONTAINER_TAG = COMPUTE_PARAMS_NO_LOG_NAME.merge(
-    log_name: CONTAINER_TAG
-  ).freeze
 
   # GAE.
   VMENGINE_PARAMS = {
@@ -512,6 +508,10 @@ module Constants
         'location' => CUSTOM_K8S_LOCATION
       )
     )
+  ).freeze
+  # Used in k8s fallback tests.
+  K8S_CONTAINER_PARAMS_FROM_FALLBACK = COMPUTE_PARAMS_NO_LOG_NAME.merge(
+    log_name: CONTAINER_TAG
   ).freeze
 
   # K8s Node.
