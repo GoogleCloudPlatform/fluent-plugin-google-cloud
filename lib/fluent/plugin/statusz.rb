232ad15f20060472dd7fa23b3676b86b4c8355d5
David B. Tucker <dbtucker@google.com>
2019-07-10 17:41:45 -0400

    Address more comments.

--- lib/fluent/plugin/statusz.rb
+++ lib/fluent/plugin/statusz.rb
@@ -31,9 +31,46 @@ end
 
 SERVER_START = Time.now
 
-# Note: The table below doesn't include the following config params
-# because they are deprecated: auth_method, private_key_email,
-# private_key_passphrase, private_key_path.
+# Does not include the following deprecated config params:
+# auth_method, private_key_email, private_key_passphrase, private_key_path
+CONFIG_KEYS = %w(
+  adjust_invalid_timestamps
+  autoformat_stackdriver_trace
+  coerce_to_utf8
+  detect_json
+  detect_subservice
+  enable_metadata_agent
+  enable_monitoring
+  http_request_key
+  insert_id_key
+  k8s_cluster_location
+  k8s_cluster_name
+  kubernetes_tag_regexp
+  label_map
+  labels_key
+  labels
+  logging_api_url
+  metadata_agent_url
+  monitoring_type
+  non_utf8_replacement_string
+  operation_key
+  partial_success
+  project_id
+  require_valid_tags
+  source_location_key
+  span_id_key
+  split_logs_by_tag
+  statusz_port
+  subservice_name
+  trace_key
+  trace_sampled_key
+  use_aws_availability_zone
+  use_grpc
+  use_metadata_service
+  vm_id
+  vm_name
+  zone
+).freeze
 
 # rubocop:disable LineLength
 STATUSZ_TMPL = %(\
@@ -81,43 +118,7 @@ STATUSZ_TMPL = %(\
     <h1>Parsed configuration</h1>
 
     <table>
-      <tr><th>adjust_invalid_timestamps</th><td><%= plugin.adjust_invalid_timestamps %></td></tr>
-      <tr><th>auth_method</th><td><%= plugin.auth_method %></td></tr>
-      <tr><th>autoformat_stackdriver_trace</th><td><%= plugin.autoformat_stackdriver_trace %></td></tr>
-      <tr><th>coerce_to_utf8</th><td><%= plugin.coerce_to_utf8 %></td></tr>
-      <tr><th>detect_json</th><td><%= plugin.detect_json %></td></tr>
-      <tr><th>detect_subservice</th><td><%= plugin.detect_subservice %></td></tr>
-      <tr><th>enable_metadata_agent</th><td><%= plugin.enable_metadata_agent %></td></tr>
-      <tr><th>enable_monitoring</th><td><%= plugin.enable_monitoring %></td></tr>
-      <tr><th>http_request_key</th><td><%= plugin.http_request_key %></td></tr>
-      <tr><th>insert_id_key</th><td><%= plugin.insert_id_key %></td></tr>
-      <tr><th>k8s_cluster_location</th><td><%= plugin.k8s_cluster_location %></td></tr>
-      <tr><th>k8s_cluster_name</th><td><%= plugin.k8s_cluster_name %></td></tr>
-      <tr><th>kubernetes_tag_regexp</th><td><%= plugin.kubernetes_tag_regexp %></td></tr>
-      <tr><th>label_map</th><td><%= plugin.label_map %></td></tr>
-      <tr><th>labels_key</th><td><%= plugin.labels_key %></td></tr>
-      <tr><th>labels</th><td><%= plugin.labels %></td></tr>
-      <tr><th>logging_api_url</th><td><%= plugin.logging_api_url %></td></tr>
-      <tr><th>metadata_agent_url</th><td><%= plugin.metadata_agent_url %></td></tr>
-      <tr><th>monitoring_type</th><td><%= plugin.monitoring_type %></td></tr>
-      <tr><th>non_utf8_replacement_string</th><td><%= plugin.non_utf8_replacement_string %></td></tr>
-      <tr><th>operation_key</th><td><%= plugin.operation_key %></td></tr>
-      <tr><th>partial_success</th><td><%= plugin.partial_success %></td></tr>
-      <tr><th>project_id</th><td><%= plugin.project_id %></td></tr>
-      <tr><th>require_valid_tags</th><td><%= plugin.require_valid_tags %></td></tr>
-      <tr><th>source_location_key</th><td><%= plugin.source_location_key %></td></tr>
-      <tr><th>span_id_key</th><td><%= plugin.span_id_key %></td></tr>
-      <tr><th>split_logs_by_tag</th><td><%= plugin.split_logs_by_tag %></td></tr>
-      <tr><th>statusz_port</th><td><%= plugin.statusz_port %></td></tr>
-      <tr><th>subservice_name</th><td><%= plugin.subservice_name %></td></tr>
-      <tr><th>trace_key</th><td><%= plugin.trace_key %></td></tr>
-      <tr><th>trace_sampled_key</th><td><%= plugin.trace_sampled_key %></td></tr>
-      <tr><th>use_aws_availability_zone</th><td><%= plugin.use_aws_availability_zone %></td></tr>
-      <tr><th>use_grpc</th><td><%= plugin.use_grpc %></td></tr>
-      <tr><th>use_metadata_service</th><td><%= plugin.use_metadata_service %></td></tr>
-      <tr><th>vm_id</th><td><%= plugin.vm_id %></td></tr>
-      <tr><th>vm_name</th><td><%= plugin.vm_name %></td></tr>
-      <tr><th>zone</th><td><%= plugin.zone %></td></tr>
+#{CONFIG_KEYS.map { |k| "      <tr><th>#{k}</th><td><%= plugin.#{k} %></td></tr>" }.join("\n")}
     </table>
   </body>
 </html>
