3c2c7148231ec407f85dd3e41775f286e9707f63
Ling Huang <lingshi@google.com>
2019-08-12 17:25:24 -0400

    Add more time keys to the preserved keys for JSON detection.

--- lib/fluent/plugin/out_google_cloud.rb
+++ lib/fluent/plugin/out_google_cloud.rb
@@ -640,6 +640,10 @@ module Fluent
             # field.
             preserved_keys = [
               'time',
+              'timeNanos',
+              'timestamp',
+              'timestampNanos',
+              'timestampSeconds',
               'severity',
               @http_request_key,
               @insert_id_key,
