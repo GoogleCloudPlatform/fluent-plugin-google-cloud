f84e2ae6a91d28d6c16dd1e1e7a664a324a82603
Javier Kohen <jkohen@google.com>
2019-08-14 13:59:01 -0400

    Introduced plugin for memory profiling

---
+++ lib/fluent/plugin/in_object_space_dump.rb
@@ -0,0 +1,52 @@
+# Copyright 2014 Google Inc. All rights reserved.
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+require 'fluent/plugin/input'
+
+module Fluent
+  # Dump out all live objects to a json file.
+  class ObjectSpaceDumpInput < Fluent::Plugin::Input
+    Fluent::Plugin.register_input('object_space_dump', self)
+
+    helpers :timer
+
+    def initialize
+      super
+    end
+
+    config_param :emit_interval, :time, default: 60
+    config_param :tag, :string
+
+    def multi_workers_ready?
+      true
+    end
+
+    def start
+      super
+
+      timer_execute(:object_space_dump_input, @emit_interval,
+                    &method(:on_timer))
+    end
+
+    def on_timer
+      GC.start
+      file = Tempfile.new(['heap', '.json'])
+      begin
+        log.info 'dumping object space to', filepath: file.path, tag: @tag
+        ObjectSpace.dump_all(output: file)
+      ensure
+        file.close
+      end
+    end
+  end
+end
