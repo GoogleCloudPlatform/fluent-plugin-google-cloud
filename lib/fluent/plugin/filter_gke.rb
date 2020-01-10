8f9499fe9b1fbdb4e10e91bb6234acc5385e1074
Javier Kohen <jkohen@google.com>
2020-01-10 16:11:09 -0500

    Introduced GKE filter.

--- lib/fluent/plugin/filter_gke.rb
+++ lib/fluent/plugin/filter_gke.rb
@@ -40,11 +40,6 @@ module Fluent
     end
 
     def filter_mode1(_tag, _time, record)
-      # Only generate and add an insertId field if the record is a hash and
-      # the insert ID field is not already set (or set to an empty string).
-      if record.is_a?(Hash) && record[@insert_id_key].to_s.empty?
-        record[@insert_id_key] = increment_insert_id
-      end
       # Extract "kubernetes"->"labels" and set them as
       # "logging.googleapis.com/labels". Prefix these labels with
       # "k8s-pod-labels" to distinguish with other labels and avoid
